8204b2bf5f38d1fab4cfc834e8de0f67f27efe54
andreia tarifa <andreia.p.tarifa@gmail.com>
2022-03-03 15:26:53 -0300

    model appointment and controller appointments creation

--- db/migrate/20220303174511_create_appointments.rb
+++ db/migrate/20220303174511_create_appointments.rb
@@ -2,8 +2,8 @@ class CreateAppointments < ActiveRecord::Migration[6.1]
   def change
     create_table :appointments do |t|
       t.date :date, null: false
-      t.references: :user, foreign_key: true, null: false
-      t.references: :treatment, foreign_key: true, null: false
+      t.references :user, foreign_key: true, null: false
+      t.references :treatment, foreign_key: true, null: false
       t.timestamps
     end
   end
