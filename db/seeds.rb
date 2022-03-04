82cc484397e1c3a10846e23743e33807a330003e
andreia tarifa <andreia.p.tarifa@gmail.com>
2022-03-04 14:56:29 -0300

    atualização no search, cards, inclusão de button de redirect

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'date'
 require 'faker'
 
@@ -23,19 +16,9 @@ first_names.each_with_index do |name, i|
   )
 end
 
-
-# 10.times do |i|
-#   User.create!(
-#     email: "teste#{i + 1}@gmail.com"
-#     encrypted_password: "pass#{rand(1000000..300000000)}"
-#     first_name: first_names.sample
-#     last_name: last_names.sample
-#   )
-# end
-
-7.times do |i|
+100.times do |i|
   Treatment.create!(
-  address: "address #{i + 10}",
+  address: "São Paulo #{i + 10}",
   specialty: specialties.sample,
   price: rand(100..500),
   availability: Date.today+rand(100),
