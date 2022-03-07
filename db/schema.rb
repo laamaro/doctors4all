c6aea82cf18b40784775258a444c7d3c960e5573
andreia tarifa <andreia.p.tarifa@gmail.com>
2022-03-07 20:23:36 -0300

    user registration edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
+
 ActiveRecord::Schema.define(version: 2022_03_07_160411) do
-=======
-ActiveRecord::Schema.define(version: 2022_03_07_135912) do
->>>>>>> master
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
