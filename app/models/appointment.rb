91a532eaee53fbdaaf74fba617ddfdee8a1dceb4
Laura Amaro <lauraamaro1804@gmail.com>
2022-03-07 15:47:00 -0300

    range de datas, falta arrumar o style

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,5 +2,14 @@ class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :treatment
 
+  validate :is_between_parent_period
   validates :date, :user, :treatment, presence: true
+
+  private
+
+  def is_between_parent_period
+    unless date.between?(self.treatment.start_date, self.treatment.end_date)
+      errors.add(:base, 'Falha ao marcar a consulta! Escolha uma data disponível!')
+    end
+  end
 end
