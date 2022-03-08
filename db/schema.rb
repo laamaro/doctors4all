0d1720502e58fc5f35b3c8c268f68d0e13e55398
michellepieroni <mipieroni@gmail.com>
2022-03-08 12:24:35 -0300

    buttons

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_03_07_160411) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,9 +55,9 @@ ActiveRecord::Schema.define(version: 2022_03_07_160411) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "end_date"
     t.float "latitude"
     t.float "longitude"
-    t.date "end_date"
     t.index ["user_id"], name: "index_treatments_on_user_id"
   end
 
